3d6c505e9ba3418c990f0725951e931ae1ae7dc4
Tim Carey-Smith <tim@spork.in>
2013-04-21 02:18:26 +1200

    Make stdout sync

--- demo.rb
+++ demo.rb
@@ -1,5 +1,7 @@
 require 'tweetstream'
 
+$stdout.sync = true
+
 Twitter.configure do |config|
   config.consumer_key       = ENV.fetch("TWITTER_KEY")
   config.consumer_secret    = ENV.fetch("TWITTER_SECRET")
